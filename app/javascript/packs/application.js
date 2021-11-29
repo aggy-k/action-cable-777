174d0e9943fdeef7d0e64379bdbbccb3f05d212c
Agnes Selvita <a.selvita@gmail.com>
2021-11-29 11:37:55 +0800

    action cable chatroom demo

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -21,6 +21,7 @@ require("channels")
 // Note(lewagon): ABOVE IS RAILS DEFAULT CONFIGURATION
 // WRITE YOUR OWN JS STARTING FROM HERE 👇
 // ----------------------------------------------------
+import { initChatroomCable } from '../channels/chatroom_channel'
 
 // External imports
 import "bootstrap";
@@ -29,6 +30,5 @@ import "bootstrap";
 // import { initSelect2 } from '../components/init_select2';
 
 document.addEventListener('turbolinks:load', () => {
-  // Call your functions here, e.g:
-  // initSelect2();
+  initChatroomCable();
 });
